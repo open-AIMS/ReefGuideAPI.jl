56290978136a3f3b5f879937169535b03a8f7a3c
Benjamin Grier <b.grier@aims.gov.au>
2024-08-28 16:40:32 +1000

    Clean up dev script and attempt at removing intersecting locations

--- src/dev_slope_alignment.jl
+++ src/dev_slope_alignment.jl
@@ -1,11 +1,33 @@
 using GLMakie, GeoMakie
 using LinearAlgebra
+using Rasters
 import ArchGDAL as AG
 import GeoInterface as GI
 import GeoDataFrames as GDF
 import GeoFormatTypes as GFT
 
+using
+    Glob,
+    TOML
+
+using DataFrames
+using OrderedCollections
+using Memoization
+using SparseArrays
+
+import GeoDataFrames as GDF
+using
+    GeoParquet,
+    Rasters
+
+using
+    FLoops,
+    HTTP,
+    Oxygen
+
+include("assessment/criteria.jl")
 include("geom_handlers/site_assessment.jl")
+include("assessment/query_thresholds.jl")
 
 reg = "Townsville-Whitsunday"
 MPA_OUTPUT_DIR = "c:/Users/bgrier/Documents/Projects/GBR-reef-guidance-assessment/outputs/MPA/"
@@ -23,26 +45,19 @@ rst_stack = (
 )
 scan_locs = joinpath(MPA_OUTPUT_DIR, "$(reg)_suitable_slopes_rugosity.tif")
 scan_locs = Raster(scan_locs)
-#scan_locs = Rasters.resample(scan_locs; crs=EPSG(7856))
 threshold = 95
 scan_locs = trim(scan_locs .> threshold)
 
 rst_stack = RasterStack(rst_stack; lazy=true)
-# rst_stack = Rasters.reproject(rst_stack; crs=EPSG(7856))
-
-# Define the polygon shape to search for (and auto-rotate)
-xs = (1, 450)
-ys = (1, 10)
-search_plot = create_poly(create_bbox(xs, ys), EPSG(7856))
-poly(search_plot)
-geom=search_plot
-geom = GO.reproject(geom, EPSG(7856), EPSG(7844))
 
 gdf = GDF.read("../canonical-reefs/output/rrap_canonical_2024-07-24-T12-38-38.gpkg")
-#gdf.geometry = AG.reproject(gdf.geometry, EPSG(7844), EPSG(7856); order=:trad)
+
 gdf.geometry = GO.simplify.(gdf.geometry; number=20)
 valid_pixels = Rasters.trim(mask(scan_locs; with=gdf.geometry))
 indices = findall(valid_pixels)
+
+x_dist = 450
+y_dist = 10
 
 function find_horiz(geom)
     coords = collect(GI.coordinates(geom)...)
@@ -75,6 +90,10 @@ function angle_cust(a, b)
     return acosd(clamp(a⋅b/(norm(a)*norm(b)), -1, 1))
 end
 
+function meters_to_degrees(x, lat)
+    return x / (111.1*1000 * cosd(lat))
+end
+
 function identify_closest_edge(pixel, reef)
     if typeof(reef) == Vector{GI.Wrappers.WrapperGeometry{false, false}}
         reef_points = GI.coordinates(reef...)
@@ -97,61 +116,109 @@ function identify_closest_edge(pixel, reef)
     return edge_line
 end
 
+function initial_search_box(lon, lat, x_dist, y_dist, res, crs, gdf)
+    lon_dist = meters_to_degrees(x_dist, lat)
+    xs = (lon - lon_dist/2, lon + lon_dist/2)
+    lat_dist = meters_to_degrees(y_dist, lat)
+    ys = (lat - lat_dist/2, lat + lat_dist/2)
 
+    search_plot = create_poly(create_bbox(xs, ys), crs)
+    search_box_line = find_horiz(search_plot)
+    geom_buff = GO.buffer(search_plot, res)
 
-function identify_potential_sites(rst_stack, scan_locs, indices, threshold, geom)
-    res = abs(step(dims(scan_locs, X)))
+    pixel = AG.createpoint()
+    pixel = AG.addpoint!(pixel, lon, lat)
+    reef = gdf[GO.within.([pixel], gdf.geometry),:].geometry
+    edge_line = identify_closest_edge(pixel, reef)
+
+    # Calculate the angle between the two lines
+    edge_bearing = angle_cust([(0.0,5.0), (0.0,0.0)], from_zero(edge_line))
+    rot_angle = angle_cust(from_zero(search_box_line), from_zero(edge_line))
+    if edge_bearing > 90
+        rot_angle = -rot_angle
+    end
+
+    return geom_buff, rot_angle
+end
+
+"""
+    identify_potential_sites(
+        rst_stack::RasterStack,
+        search_pixels::Raster,
+        indices::Vector{CartesianIndex{2}},
+        gdf::DataFrame,
+        x_dist::Union{Int64, Float64},
+        y_dist::Union{Int64, Float64}
+    )::DataFrame
+
+Identify the most suitable site polygons for each pixel in the `search_pixels` raster where
+`indices` denotes which pixels to check for suitability. `x_dist` and `y_dist` are x and y
+lengths of the search polygon. A buffer of `rst_stack` resolution is applied to the search box.
+
+# Arguments
+- `rst_stack` : RasterStack containing environmental variables for assessment.
+- `search_pixels` : Raster that matches indices for lon/lat information.
+- `indices` : Vector of CartesianIndices noting pixels to assess sites.
+- `gdf` : GeoDataFrame containing the reef outlines used to align the search box edge.
+- `x_dist` : Length of horizontal side of search box.
+- `y_dist` : Length of vertical side of search box.
+- `crs` : CRS of the input Rasters. Using GeoFormatTypes.EPSG().
+
+# Returns
+DataFrame containing highest score, rotation and polygon for each assessment at pixels in indices.
+"""
+function identify_potential_sites(
+    rst_stack::RasterStack,
+    indices_pixels::Raster,
+    indices::Vector{CartesianIndex{2}},
+    gdf::DataFrame,
+    x_dist::Union{Int64, Float64},
+    y_dist::Union{Int64, Float64},
+    crs::GeoFormatTypes.CoordinateReferenceSystemFormat
+)::DataFrame
+    res = abs(step(dims(rst_stack, X)))
     #geom_buff = GO.simplify(geom_buff; number = 4) # simplify buffer to 4 vertex polygon
 
     # # TODO: Dynamically build this ruleset
-    # ruleset = Dict(
-    #     :Depth => (data) -> within_thresholds(data, -9.0, -2.0),
-    #     :WavesTp => (data) -> within_thresholds(data, 0.0, 5.9)
-    # )
+    ruleset = Dict(
+         :Depth => (data) -> within_thresholds(data, -9.0, -2.0),
+         :WavesTp => (data) -> within_thresholds(data, 0.0, 5.9)
+    )
 
     # Search each location to assess
     best_score = zeros(length(indices))
     best_poly = Vector(undef, length(indices))
-    best_degree = zeros(Int64, length(indices))
-    for (i, index) in enumerate(indices)
+    best_rotation = zeros(Int64, length(indices))
+    @floop for (i, index) in enumerate(indices)
         # Move geom to new centroid
-        lon = dims(valid_pixels, X)[index[1]]
-        lat = dims(valid_pixels, Y)[index[2]]
+        lon = dims(indices_pixels, X)[index[1]]
+        lat = dims(indices_pixels, Y)[index[2]]
+        geom_buff, rot_angle = initial_search_box(lon, lat, x_dist, y_dist, res, crs, gdf)
 
-        m_coords = AG.reproject([lon, lat], GFT.EPSG(7844), GFT.EPSG(7856); order=:trad)
-        mv_geom = move_geom(geom, (m_coords[1], m_coords[2]))
-        mv_geom = GO.reproject(mv_geom; source_crs=EPSG(7856), target_crs=EPSG(7844))
-        search_box_line = find_horiz(mv_geom)
-        geom_buff = GO.buffer(mv_geom, res)
-
-        pixel = AG.createpoint()
-        pixel = AG.addpoint!(pixel, lon, lat)
-        reef = gdf[GO.within.([pixel], gdf.geometry),:].geometry
-        edge_line = identify_closest_edge(pixel, reef)
-
-        # Calculate the angle between the two lines
-        edge_bearing = angle_cust([(0.0,5.0), (0.0,0.0)], from_zero(edge_line))
-        rot_angle = angle_cust(from_zero(search_box_line), from_zero(edge_line))
-        if edge_bearing > 90
-            rot_angle = -rot_angle
-        end
-
-        b_score, b_deg, b_poly = assess_reef_site(rst_stack, mv_geom, ruleset; degree_step=15.0, start_rot=rot_angle)
+        b_score, b_rot, b_poly = assess_reef_site(
+            rst_stack,
+            geom_buff,
+            ruleset;
+            degree_step=15.0,
+            start_rot=rot_angle,
+            n_per_side=2
+        )
 
         best_score[i] = b_score
-        best_degree[i] = b_deg
+        best_rotation[i] = b_rot
         best_poly[i] = b_poly
     end
 
-    return DataFrame(score=best_score, orientation=best_degree, poly=best_poly)
+    return DataFrame(score=best_score, orientation=best_rotation, poly=best_poly)
 end
 
-function assess_reef_site(rst, geom, ruleset; degree_step=15.0, start_rot)
-    n_rotations = length(start_rot-degree_step:degree_step:start_rot+degree_step)
+function assess_reef_site(rst, geom, ruleset; degree_step=15.0, start_rot=0.0, n_per_side=1)
+    rotations = start_rot-(degree_step*n_per_side):degree_step:start_rot+(degree_step*n_per_side)
+    n_rotations = length(rotations)
     score = zeros(n_rotations)
     best_poly = Vector(undef, n_rotations)
 
-    for (j, r) in enumerate(start_rot-degree_step:degree_step:start_rot+degree_step)
+    for (j, r) in enumerate(rotations)
         rot_geom = rotate_geom(geom, r)
         c_rst = crop(rst; to=rot_geom)
         if !all(size(c_rst) .> (0, 0))
@@ -172,5 +239,13 @@ function assess_reef_site(rst, geom, ruleset; degree_step=15.0, start_rot)
         best_poly[j] = rot_geom
     end
 
-    return score[argmax(score)], argmax(score)-1, best_poly[argmax(score)]
+    return score[argmax(score)], argmax(score), best_poly[argmax(score)]
 end
+
+TSV_95_slopes = identify_potential_sites(rst_stack, valid_pixels, indices, gdf, 450, 10, EPSG(7844))
+reduced_df = TSV_95_slopes
+for (ind, row) in enumerate(eachrow(TSV_95_slopes))
+    intersecting_polys = TSV_95_slopes[(GO.intersects.([row.poly], TSV_95_slopes[:, :poly])), :]
+    reduced_df[ind, :] = TSV_95_slopes[argmax(intersecting_polys.score), :]
+end
+test = unique(TSV_95_slopes)
