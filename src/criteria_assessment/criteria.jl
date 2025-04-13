d01ee40bff255ca39eccf06c1c7e4c820be0ac30
Takuya Iwanaga <takuyai@gmail.com>
2025-04-13 16:27:42 +1000

    Ignore unused datasets

--- src/criteria_assessment/criteria.jl
+++ src/criteria_assessment/criteria.jl
@@ -29,9 +29,11 @@ function criteria_data_map()
         :WavesTp => "_waves_Tp",
         :Rugosity => "_rugosity",
         :ValidSlopes => "_valid_slopes",
-        :ValidFlats => "_valid_flats",
-        :PortDistSlopes => "_PortDistSlopes",
-        :PortDistFlats => "_PortDistFlats"
+        :ValidFlats => "_valid_flats"
+
+        # Unused datasets
+        # :PortDistSlopes => "_PortDistSlopes",
+        # :PortDistFlats => "_PortDistFlats"
     )
 end
 
